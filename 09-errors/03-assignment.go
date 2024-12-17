83e21b52205404b155a9636fbc8e17e55260b4d3
Magesh Kuppan <tkmagesh77@gmail.com>
2024-12-17 14:11:46 +0530

    error handling - assignment

---
+++ 09-errors/03-assignment.go
@@ -0,0 +1,72 @@
+/*
+Handle the edge case scenarios using errors (userChoice)
+*/
+
+package main
+
+import "fmt"
+
+func main() {
+	var userChoice int
+	for {
+		userChoice = getUserChoice()
+		if userChoice == 5 {
+			break
+		}
+		if userChoice < 1 || userChoice > 5 {
+			fmt.Println("Invalid choice")
+			continue
+		}
+		processUserChoice(userChoice)
+	}
+}
+
+func processUserChoice(userChoice int) {
+	var result int
+	n1, n2 := getOperands()
+	switch userChoice {
+	case 1:
+		result = add(n1, n2)
+	case 2:
+		result = subtract(n1, n2)
+	case 3:
+		result = multiply(n1, n2)
+	case 4:
+		result = divide(n1, n2)
+	}
+	fmt.Println("Result :", result)
+}
+
+func add(x, y int) int {
+	return x + y
+}
+
+func subtract(x, y int) int {
+	return x - y
+}
+
+func multiply(x, y int) int {
+	return x * y
+}
+
+func divide(x, y int) int {
+	return x / y
+}
+
+func getOperands() (n1, n2 int) {
+	fmt.Println("Enter the operands")
+	fmt.Scanln(&n1, &n2)
+	return
+}
+
+func getUserChoice() int {
+	var userChoice int
+	fmt.Println("1. Add")
+	fmt.Println("2. Subtract")
+	fmt.Println("3. Multiply")
+	fmt.Println("4. Divide")
+	fmt.Println("5. Exit")
+	fmt.Println("Enter the choice:")
+	fmt.Scanln(&userChoice)
+	return userChoice
+}
