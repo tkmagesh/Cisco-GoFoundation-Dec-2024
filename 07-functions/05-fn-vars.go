3f474053609c311709a10f37480b8b5eff3fd5f8
Magesh Kuppan <tkmagesh77@gmail.com>
2024-12-17 10:12:14 +0530

    function variables

--- 07-functions/05-fn-vars.go
+++ 07-functions/05-fn-vars.go
@@ -15,4 +15,32 @@ func main() {
 		fmt.Println("Hi!")
 	}
 	sayHi()
+
+	var sayHello func(string)
+	sayHello = func(userName string) {
+		fmt.Printf("Hello %q!\n", userName)
+	}
+	sayHello("Magesh")
+
+	var greet func(string, string)
+	greet = func(firstName, lastName string) {
+		fmt.Printf("Hi %s %s, Have a nice day!\n", firstName, lastName)
+	}
+	greet("Magesh", "Kuppan")
+
+	var getGreet func(string, string) string
+	getGreet = func(firstName, lastName string) string {
+		return fmt.Sprintf("Hi %s %s, Have a good day!", firstName, lastName)
+	}
+	msg := getGreet("Suresh", "Kannan")
+	fmt.Println(msg)
+
+	var divide func(int, int) (int, int)
+	divide = func(x, y int) (quotient, remainder int) { //memory allocated & initialized for quotient & remainder
+		quotient = x / y
+		remainder = x % y
+		return
+	}
+	q, r := divide(100, 7)
+	fmt.Printf("Dividing 100 by 7, quotient = %d and remainder = %d\n", q, r)
 }
