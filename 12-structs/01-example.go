c3e7f8c228909a26040b9c62cf6b85ad303dc586
Magesh Kuppan <tkmagesh77@gmail.com>
2024-12-17 16:50:09 +0530

    updates

--- 12-structs/01-example.go
+++ 12-structs/01-example.go
@@ -40,18 +40,20 @@ func main() {
 	fmt.Printf("p.Cost = %v, p2.Cost = %v\n", p.Cost, p2.Cost)
 
 	fmt.Println("Before applying 10% discount:")
-	fmt.Println(Format( /* ? */ ))
-	ApplyDiscount( /* ? */ ) // apply 10% discount on the proeduct (p)
+	fmt.Println(Format(p))
+	ApplyDiscount(&p, 10) // apply 10% discount on the proeduct (p)
 	fmt.Println("After applying 10% discount:")
-	fmt.Println(Format( /* ? */ ))
+	fmt.Println(Format(p))
 
 }
 
-func Format() string {
+func Format(p Product) string {
 	// return a formatted string of the given product
 	// ex : Id = 100, Name = "Pen", Cost = 10.00
+	return fmt.Sprintf("Id = %d, Name = %q, Cost = %0.2f", p.Id, p.Name, p.Cost)
 }
 
-func ApplyDiscount( /* ? */ ) { // no return values
+func ApplyDiscount(p *Product, discountPercentage float64) { // no return values
 	// update the cost of the given product by applying the given discount
+	p.Cost = p.Cost * ((100 - discountPercentage) / 100)
 }
